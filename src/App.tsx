eb289efaf10e08dc69b2eeed0046a58c1042bed8
Vladislav Korbut <v.korbut8@gmail.com>
2018-03-06 09:14:50 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component<object, {}> {
+  render() {
+    return (
+      <div>
+        123
+      </div>
+    );
+  }
+}
+
+export default App;
