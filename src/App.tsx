22ee326db75324cbeb2a30179485c75dcdbe6449
Vladislav Korbut <v.korbut8@gmail.com>
2018-03-07 13:01:47 +0300

    Add images resizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import Gallery from './components/Gallery';
+import './index.css';
 
 class App extends React.Component<object, {}> {
   render() {
     return (
       <div>
-        123
+        <Gallery />
       </div>
     );
   }
